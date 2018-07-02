91aa223b4a5537bd5bd7be8a888494e85d6bb228
phuongcitigo <phuong.nguyenhuy@citigo.com.vn>
2018-07-02 14:50:15 +0700

    fix bash

--- mysite/celery.py
+++ mysite/celery.py
@@ -5,7 +5,7 @@ from celery import Celery
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
-app = Celery('mysite', broker='amqp://user:password@rabbit1:5672')
+app = Celery('mysite', broker='amqp://user:password@rabbit1:5672', backend='amqp://user:password@rabbit1:5672')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
--- run_celery.sh
+++ run_celery.sh
@@ -3,8 +3,12 @@
 # wait for RabbitMQ server to start
 sleep 10
 
-cd mysite
-# run Celery worker for our project mysite with Celery configuration stored in Celeryconf
-# su -m myuser -c "celery worker -A mysite.celeryconf -Q default -n default@%h"
+#cd mysite
+## run Celery worker for our project mysite with Celery configuration stored in Celeryconf
+## su -m myuser -c "celery worker -A mysite.celeryconf -Q default -n default@%h"
+#
+#su -m myuser -c "celery -A mysite worker --loglevel=info"
 
-su -m myuser -c "celery -A mysite worker --loglevel=info"
+
+#celery -A mysite worker -B -E -l $debugLevel --concurrency=2 --autoreload
+celery -A mysite worker --concurrency=20 --loglevel=info
